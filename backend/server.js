ba5d99bfaa8ae47b0fdcf1792e79a5996f5ab777
Gordon <gordonj2016@outlook.com>
2025-07-19 14:38:20 -0500

    port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ mongoose
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(
